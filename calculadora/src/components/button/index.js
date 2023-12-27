fa73761ab7a28ecc889febecfe2783a53c72bda7
gabriel <bielh2001@gmail.com>
2023-12-26 21:55:21 -0300

    att

--- calculadora/src/components/button/index.js
+++ calculadora/src/components/button/index.js
@@ -1,11 +1,12 @@
-const Button= () => {
+
+import { ButtonContainer } from './styles';
+
+const Button = ({label, onClick}) => {
     return (
-      <div className="App">
-        olá
-   
-      </div>
+      <ButtonContainer onClick={onClick} type="button">
+       {label}
+      </ButtonContainer>
     );
   }
   
   export default Button;
-  
